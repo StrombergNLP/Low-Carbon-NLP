76eb4eaee04c3933dfee66d564736ef14cb68e0a
mkon <mkon@itu.dk>
2021-02-20 13:31:44 +0100

    small

--- src/experiment.py
+++ src/experiment.py
@@ -15,11 +15,11 @@ def main():
     with open(path + '/config.json') as json_file:
         config = json.load(json_file)
 
-        dataset = load_dataset(config['dataset'], script_version='master')
+        dataset = load_dataset(config['dataset'])
         dataset_train = dataset['train']
         tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
 
-        dataset_reduced = dataset_train['text'][:128]
+        dataset_reduced = dataset_train['text']
 
         inputs = tokenizer.batch_encode_plus(
             dataset_reduced, truncation=True, padding=True, verbose=True
