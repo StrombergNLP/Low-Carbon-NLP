26ce50f6732f7c0dad9aa15ec615cd805ef59ee3
mkon <mkon@itu.dk>
2021-02-20 15:04:21 +0100

    more callback

--- src/callbacks/CarbonTrackerCallback.py
+++ src/callbacks/CarbonTrackerCallback.py
@@ -6,11 +6,11 @@ class CarbonTrackerCallback(TrainerCallback):
         super().__init__()
         self.tracker = CarbonTracker(epochs=max_epochs)
 
-    def on_epoch_begin(self, **kwargs):
+    def on_epoch_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         self.tracker.epoch_start()
 
-    def on_epoch_end(self, **kwargs):
+    def on_epoch_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         self.tracker.epoch_end()
 
-    def on_train_end(self, **kwargs):
+    def on_train_end(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         self.tracker.stop()
