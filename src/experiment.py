baedafb2c339756bc0c2cf4d04ea90a5adce20bd
Lucas Puvis de Chavannes <lupu@itu.dk>
2021-02-21 13:32:17 +0100

    Updated config to include optimizer settings

--- src/experiment.py
+++ src/experiment.py
@@ -4,6 +4,7 @@ import json
 from transformers import Trainer, TrainingArguments
 from transformers import RobertaTokenizerFast
 from transformers import DataCollatorForLanguageModeling, LineByLineTextDataset
+from transformers import AdamW
 from datasets import load_dataset
 from model.RoBERTaModel import RoBERTaModel
 from callbacks.CarbonTrackerCallback import CarbonTrackerCallback
@@ -14,33 +15,34 @@ def main():
 
     with open(path + '/config.json') as json_file:
         config = json.load(json_file)
+        epochs = config['train_epochs']
 
-        dataset = load_dataset(config['dataset'])
+
+        dataset = load_dataset(config['dataset'], script_version='master')
         dataset_train = dataset['train']
         tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
 
-        dataset_reduced = dataset_train['text']
-
+        dataset_reduced = dataset_train['text'][:128]
         inputs = tokenizer.batch_encode_plus(
             dataset_reduced, truncation=True, padding=True, verbose=True
         )
-
         data_collator = DataCollatorForLanguageModeling(
             tokenizer=tokenizer, mlm=True, mlm_probability=0.15
         )
 
         model = RoBERTaModel(config['model_parameters'][0])
 
-        epochs = config['train_epochs']
-        batch_size = config['batch_size']
+
+        opt_param = config['optimizer_parameters']
+        optimizer = AdamW(lr=opt_param['lr'], betas=(opt_param['beta_one'], opt_param['beta_two']), eps=opt_param['eps'], weight_decay=opt_param['weight_decay'])
 
         training_args = TrainingArguments(
             output_dir='./results',
             num_train_epochs=epochs,
-            per_device_train_batch_size=batch_size,
-            per_device_eval_batch_size=batch_size,
-            warmup_steps=500,
-            weight_decay=0.01,
+            per_device_train_batch_size=config['batch_size'],
+            per_device_eval_batch_size=config['batch_size'],
+            warmup_steps=opt_param['warmup_steps'],
+            weight_decay=opt_params['weight_decay'],
             logging_dir='./logs'
         )
 
