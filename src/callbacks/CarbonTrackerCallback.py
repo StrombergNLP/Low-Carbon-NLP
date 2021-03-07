c1cf7414c2c0a5291571f37c6760ae609d8468f2
Lucas Puvis de Chavannes <lupu@itu.dk>
2021-03-07 11:05:07 +0100

    lol

--- src/callbacks/CarbonTrackerCallback.py
+++ src/callbacks/CarbonTrackerCallback.py
@@ -4,7 +4,9 @@ from carbontracker.tracker import CarbonTracker
 class CarbonTrackerCallback(TrainerCallback):
     def __init__(self, max_epochs):
         super().__init__()
-        self.tracker = CarbonTracker(epochs=max_epochs)
+        self.tracker = CarbonTracker(epochs=max_epochs, epochs_before_pred=-1, monitor_epochs=-1)
+        energy_usages = self.tracker.tracker.total_energy_per_epoch()
+        energy = energy_usages.sum()
 
     def on_epoch_begin(self, args: TrainingArguments, state: TrainerState, control: TrainerControl, **kwargs):
         self.tracker.epoch_start()
