9ac6fde6e09344f8350ad975174dc4bc307bcca2
mkon <madsgkkongsbak@hotmail.com>
2021-03-12 16:39:53 +0100

    lmao maybe?

---
+++ src/test.py
@@ -0,0 +1,18 @@
+import os
+import transformers
+import sys
+
+from transformers import pipeline
+
+def main():
+fill_mask = pipeline(
+    "fill-mask",
+    model="./trained_model/pytorch_model",
+    tokenizer="roberta-base"
+)
+
+fill_mask("Let's get together and <mask> sometime!")
+
+if __name__ == '__main__':
+    main()
+
