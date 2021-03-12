a8857f4c7c0fbb1406c51b7114502d5b64874b89
mkon <madsgkkongsbak@hotmail.com>
2021-03-12 16:42:52 +0100

    im retard2

--- src/test.py
+++ src/test.py
@@ -7,7 +7,7 @@ from transformers import pipeline
 def main():
     fill_mask = pipeline(
         "fill-mask",
-        model="./trained_model/pytorch_model",
+        model="./trained_model",
         tokenizer="roberta-base"
     )
 
