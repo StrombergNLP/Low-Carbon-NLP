39a5950022e2ae80df5fb8206ebf70376b4ef0ef
mkon <madsgkkongsbak@hotmail.com>
2021-03-12 16:41:55 +0100

    im retard

--- src/test.py
+++ src/test.py
@@ -5,13 +5,13 @@ import sys
 from transformers import pipeline
 
 def main():
-fill_mask = pipeline(
-    "fill-mask",
-    model="./trained_model/pytorch_model",
-    tokenizer="roberta-base"
-)
+    fill_mask = pipeline(
+        "fill-mask",
+        model="./trained_model/pytorch_model",
+        tokenizer="roberta-base"
+    )
 
-fill_mask("Let's get together and <mask> sometime!")
+    fill_mask("Let's get together and <mask> sometime!")
 
 if __name__ == '__main__':
     main()
