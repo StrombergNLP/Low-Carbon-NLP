ad059ed73116339f2fdad6bc902f8597e5b50ee0
Lucas Puvis de Chavannes <lupu@itu.dk>
2021-03-29 15:26:05 +0200

    Slurm job ID

--- src/save_dataset.py
+++ src/save_dataset.py
@@ -2,9 +2,9 @@ from datasets import load_dataset
 
 dataset = load_dataset('cc_news', script_version='master')
 
-dataset_reduced = dataset['train']['text'][:100000]
+dataset_reduced = dataset['train']['text'][:8000]
 del dataset
 
-with open('cc_news_reduced.txt', 'w+', encoding='utf8') as datafile:
+with open('cc_news_reduced_2.txt', 'w+', encoding='utf8') as datafile:
     datafile.writelines(dataset_reduced)
 
