c376577d099afa9650c70ac43b249a8acdc2b9fe
mkon <madsgkkongsbak@hotmail.com>
2021-04-13 12:27:03 +0200

    test

--- tools/carbon_tracker_test.py
+++ tools/carbon_tracker_test.py
@@ -15,6 +15,6 @@ for i in range(epochs):
     tracker.epoch_end()
     time.sleep(2)
     logs = parser.parse_all_logs(log_dir=log_path)
-    print(logslogs[len(logs)-1]['actual'])
+    print(logs[len(logs)-1]['actual'])
 
 tracker.stop()
