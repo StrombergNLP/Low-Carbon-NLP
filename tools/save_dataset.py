8ec5ba036ad55f39b1bb2e47b7bfe11f6c8493af
mkon <madsgkkongsbak@hotmail.com>
2021-04-13 16:42:27 +0200

    param time

--- tools/save_dataset.py
+++ tools/save_dataset.py
@@ -3,6 +3,7 @@ from datasets import load_dataset
 dataset = load_dataset('cc_news', script_version='master')
 
 dataset_reduced = dataset['train']['text'][:100000]
+print(len(dataset_reduced))
 del dataset
 
 with open('cc_news_reduced.txt', 'w+', encoding='utf8') as datafile:
